4a8091f77981d307f2be93d425ebfd439d183ef0
EduGollner0609 <dudugollner@gmail.com>
2025-02-27 02:15:59 -0300

    Abrindo ocupando toda largura e altura do monitor.

--- src/main/java/org/example/stockmanager/Application.java
+++ src/main/java/org/example/stockmanager/Application.java
@@ -2,6 +2,7 @@ package org.example.stockmanager;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,11 +11,20 @@ public class Application extends javafx.application.Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("gui/application.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1149, 663);
+        Scene scene = new Scene(fxmlLoader.load());
+
+        Screen screen = Screen.getPrimary();
+        double screenWidth = screen.getVisualBounds().getWidth();
+        double screenHeight = screen.getVisualBounds().getHeight();
+
+        stage.setWidth(screenWidth);
+        stage.setHeight(screenHeight);
         stage.setScene(scene);
         stage.show();
     }
+
 
     public static void main(String[] args) {
         launch();
